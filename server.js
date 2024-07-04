5998eddf7db67044e465df1e1991491a42b0cf4c
MarcoCoppola135 <marcocoppola135@gmail.com>
2024-07-04 09:37:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/MarcoDraw', express.static(path.join(__dirname, 'MarcoDraw')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'Gallery.html'));
